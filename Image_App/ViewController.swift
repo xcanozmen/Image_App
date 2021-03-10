e47ca884493cd782540eb83c75bb5acaf05001c0
xcanozmen <can35ozmen@gmail.com>
2021-03-10 13:43:15 +0300

    Initial Commit

---
+++ Image_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image_App
+//
+//  Created by Can on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
